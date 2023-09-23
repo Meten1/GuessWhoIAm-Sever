52a204f0ed03cc9b0ba24c553ffde6620be0d368
Meten1 <xiangyu.huo@my.jcu.edu.au>
2023-09-23 11:42:10 +0800

    Add the Technical Documentation.

---
+++ Technical Documentation/member-search-index.js
@@ -0,0 +1,59 @@
+memberSearchIndex = [{
+    "p": "<Unnamed>",
+    "c": "Sever",
+    "l": "creatNewRoom(String, String)",
+    "url": "creatNewRoom(java.lang.String,java.lang.String)"
+}, {
+    "p": "<Unnamed>",
+    "c": "Sever",
+    "l": "entryRoom(String, String)",
+    "url": "entryRoom(java.lang.String,java.lang.String)"
+}, {"p": "<Unnamed>", "c": "Sever", "l": "exitGame(String)", "url": "exitGame(java.lang.String)"}, {
+    "p": "<Unnamed>",
+    "c": "Sever",
+    "l": "getCardsMessage()"
+}, {
+    "p": "<Unnamed>",
+    "c": "Sever",
+    "l": "getRandomID(String)",
+    "url": "getRandomID(java.lang.String)"
+}, {"p": "<Unnamed>", "c": "Sever", "l": "GettingMessageSQS()"}, {
+    "p": "<Unnamed>",
+    "c": "Sever",
+    "l": "inGame(String)",
+    "url": "inGame(java.lang.String)"
+}, {"p": "<Unnamed>", "c": "Sever", "l": "inputUrl"}, {
+    "p": "<Unnamed>",
+    "c": "StartRun",
+    "l": "main(String[])",
+    "url": "main(java.lang.String[])"
+}, {
+    "p": "<Unnamed>",
+    "c": "Sever",
+    "l": "messageSent1(String, String, String)",
+    "url": "messageSent1(java.lang.String,java.lang.String,java.lang.String)"
+}, {
+    "p": "<Unnamed>",
+    "c": "Sever",
+    "l": "messageSent2(String, String, String)",
+    "url": "messageSent2(java.lang.String,java.lang.String,java.lang.String)"
+}, {"p": "<Unnamed>", "c": "Sever", "l": "outputUrl1"}, {
+    "p": "<Unnamed>",
+    "c": "Sever",
+    "l": "outputUrl2"
+}, {"p": "<Unnamed>", "c": "Sever", "l": "position"}, {"p": "<Unnamed>", "c": "Sever", "l": "rooms"}, {
+    "p": "<Unnamed>",
+    "c": "Sever",
+    "l": "roundOver(String, String[])",
+    "url": "roundOver(java.lang.String,java.lang.String[])"
+}, {
+    "p": "<Unnamed>",
+    "c": "Sever",
+    "l": "Sever(String, String, String, String, Region)",
+    "url": "%3Cinit%3E(java.lang.String,java.lang.String,java.lang.String,java.lang.String,software.amazon.awssdk.regions.Region)"
+}, {"p": "<Unnamed>", "c": "Sever", "l": "startGame(String)", "url": "startGame(java.lang.String)"}, {
+    "p": "<Unnamed>",
+    "c": "StartRun",
+    "l": "StartRun()",
+    "url": "%3Cinit%3E()"
+}]
---
+++ Technical Documentation/type-search-index.js
@@ -0,0 +1,4 @@
+typeSearchIndex = [{"l": "All Classes", "url": "allclasses-index.html"}, {
+    "p": "<Unnamed>",
+    "l": "Sever"
+}, {"p": "<Unnamed>", "l": "StartRun"}]
